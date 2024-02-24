71360ecec2cd2a5fb1ef20eed0fafe30da051c90
User <utkirsobirov2000@gmail.com>
2024-02-24 23:51:06 +0500

    fix bug

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ from .cdn.conf import(
 )
 from pathlib import Path
 import os
+import logging
 import environ
 from datetime import timedelta
 import dj_database_url
@@ -304,61 +305,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "users.User"
 
 
-# LOGGING = {
-#     "version": 1,
-#     "disable_exiting_loggers": False,
-#     "filters": {
-#         "require_debug_false": {
-#             "()": "django.utils.log.RequireDebugFalse",
-#         },
-#         "require_debug_true": {
-#             "()": "django.utils.log.RequireDebugTrue",
-#         },
-
-#     },
-#     "formatters": {
-#         "django.server": {
-#             "()": "django.utils.log.ServerFormatter",
-#             "format": "[%(server_time)s] %(message)s",
-#         }
-#     },
-#     "handlers": {
-#         "console": {
-#             "level": "INFO",
-#             "filters": ["require_debug_false"],
-#             "class": "logging.StreamHandler",
-#         },
-#         "django.server": {
-#             "level": "INFO",
-#             "class": "logging.StreamHandler",
-#             "formatter": "django.server",
-#         },
-
-#         "mail_admins": {
-#             "level": "ERROR",
-#             "filters": ["require_debug_false"],
-#             "class": "django.utils.log.AdminEmailHandler",
-#         },
-
-#         "logfile": {
-#             "class": "logging.FileHandler",
-#             "filename": "server.log",
-#         },
-#     },
-
-#     "loggers": {
-#         "django": {
-#             "handlers": [
-#                 "console",
-#                 "console_debug_false",
-#                 "logfile",
-#             ],
-#             "level": "INFO",
-#         },
-#         "django.server": {
-#             "handlers": ["django.server"],
-#             "level": "INFO",
-#             "propagate": False,
-#         },
-#     },
-# }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
