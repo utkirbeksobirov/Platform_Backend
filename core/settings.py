b2ba4e67924838bc3dc4d72e411e8b46b0979aff
User <utkirsobirov2000@gmail.com>
2024-02-26 02:09:49 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -143,11 +143,11 @@ if DEBUG is True:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
 }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+elif len(sys.argv) > 0 and sys.argv[1] != "collectstatic":
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get('DATABASE_URL')),
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
@@ -246,7 +246,6 @@ AWS_S3_SECURE_URLS = True
 
 
 # rest-faramework
-
 REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "rest_framework.views.exception_handler",
     "DEFAULT_AUTHENTICATION_CLASSES": (
