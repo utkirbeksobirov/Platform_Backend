ed795a41678151daa498482989de450a5c6da5ae
User <utkirsobirov2000@gmail.com>
2024-02-29 04:10:31 +0500

    fix all

--- core/cdn/backends.py
+++ core/cdn/backends.py
@@ -1,7 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-import boto3
 
-client = boto3.client('amplifybackend')
 
 class StaticRootS3Boto3Storages(S3Boto3Storage):
     location = 'static'
--- core/cdn/conf.py
+++ core/cdn/conf.py
@@ -1,11 +1,10 @@
 import os
+import boto3
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME')
-AWS_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
-AWS_ENDPOINT = "fra1.digitaloceanspaces.com"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "noxonfx"
+AWS_ENDPOINT_URL = "https://noxonfx.fra1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
