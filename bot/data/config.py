cda3ec4bd2ef4a6907dc04d476d48b8ba4b89e96
User <utkirsobirov2000@gmail.com>
2024-03-01 04:27:27 +0500

    Update Config Bot

--- bot/data/config.py
+++ bot/data/config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '6279484912:AAEwbq7se3R3xUWKmMIh9OOp-h8IW6OSGiI'
+BOT_TOKEN = '7148916469:AAGHzKQnphBqPPAB95_tUw_H-WIpJJBFIRs'
 ADMINS = [1776054971]  # add your id [21421342,243124134]
 ADDRES = 'https://walrus-app-8p5bd.ondigitalocean.app/'
